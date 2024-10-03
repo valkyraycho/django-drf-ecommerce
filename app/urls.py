85bad59182d2977e1885aa0118268a848612b2d4
valkyraycho <razor0424@gmail.com>
2024-10-03 08:35:42 -0700

    add views and urls

--- app/urls.py
+++ app/urls.py
@@ -1,6 +1,19 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
+from rest_framework.routers import DefaultRouter
+
+from product import views
+
+router = DefaultRouter()
+router.register(r"category", views.CategoryView)
+router.register(r"brand", views.BrandView)
+router.register(r"product", views.ProductView)
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("api/", include(router.urls)),
+    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
+    path("api/schema/docs/", SpectacularSwaggerView.as_view(url_name="schema")),
 ]
--- product/views.py
+++ product/views.py
@@ -1,1 +1,34 @@
-# Create your views here.
+from django.http import HttpRequest
+from drf_spectacular.utils import extend_schema
+from rest_framework import viewsets
+from rest_framework.response import Response
+
+from .models import Brand, Category, Product
+from .serializers import BrandSerializer, CategorySerializer, ProductSerializer
+
+
+class CategoryView(viewsets.ViewSet):
+    queryset = Category.objects.all()
+
+    @extend_schema(responses=CategorySerializer)
+    def list(self, request: HttpRequest) -> Response:
+        serializer = CategorySerializer(self.queryset, many=True)
+        return Response(serializer.data)
+
+
+class BrandView(viewsets.ViewSet):
+    queryset = Brand.objects.all()
+
+    @extend_schema(responses=BrandSerializer)
+    def list(self, request: HttpRequest) -> Response:
+        serializer = BrandSerializer(self.queryset, many=True)
+        return Response(serializer.data)
+
+
+class ProductView(viewsets.ViewSet):
+    queryset = Product.objects.all()
+
+    @extend_schema(responses=ProductSerializer)
+    def list(self, request: HttpRequest) -> Response:
+        serializer = ProductSerializer(self.queryset, many=True)
+        return Response(serializer.data)
