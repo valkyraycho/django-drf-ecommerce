04bb27c4aa03bf33520f3da8afa996e8de963897
valkyraycho <razor0424@gmail.com>
2024-10-03 08:02:45 -0700

    register models to admin

--- product/admin.py
+++ product/admin.py
@@ -1,1 +1,7 @@
-# Register your models here.
+from django.contrib import admin
+
+from .models import Brand, Category, Product
+
+admin.site.register(Brand)
+admin.site.register(Category)
+admin.site.register(Product)
